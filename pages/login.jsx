456e6a5a488ad6519279ac677967f35335c99df3
Yibo Pan <yibopan12@gmail.com>
2020-02-28 16:45:34 -0600

    log in page

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {GoogleLogin} from 'react-google-login';
 
+const url = ''
+
+async function postData(url, options) {
+    const response = await fetch(url, options)
+    return await response.json()
+}
+
 const responseGoogle = (response) => {
-    console.log(response);
+    let tokenId = response.tokenId
+    const options = {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json',
+        }, 
+        body: JSON.stringify(tokenId)
+    }
+    postData(url, options)
 }
 
 const login = () => {
     return(
         <GoogleLogin
             clientId="996394968992-rm1fqmg6kakboe98mntbmtd9imgralch.apps.googleusercontent.com"
-            
             buttonText="Sign in with Google"
             onSuccess={responseGoogle}
             onFailure={responseGoogle}
@@ -17,8 +31,5 @@ const login = () => {
         />
     )
 }
-
-
-
 
 export default login;
